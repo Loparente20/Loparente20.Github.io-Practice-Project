d6adee5c913b312d3ee68def63bed1e33bf9f5b5
Laura P <loparente20@blakeschool.org>
2018-09-07 17:40:45 -0500

    Scale as well

---
+++ Assets/Scale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Widen the object by 0.1
+        transform.localScale += new Vector3(0.1F, .1f, 0);
+
+    }
+}
